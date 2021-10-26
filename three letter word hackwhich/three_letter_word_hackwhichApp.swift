8134c87ec408ee913a0797896978722b8a2aca8c
Peter Shrader <pshrader@bsd220.org>
2021-10-26 09:55:03 -0500

    Initial Commit

---
+++ three letter word hackwhich/three_letter_word_hackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  three_letter_word_hackwhichApp.swift
+//  three letter word hackwhich
+//
+//  Created by Student on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct three_letter_word_hackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
